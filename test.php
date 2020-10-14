51c07af319ea2467cd379f885a7ffdf42b31b869
Samson Adejoro <sadejoro@sbsc.com>
2020-10-14 14:33:47 +0100

    Initial commit

---
+++ test.php
@@ -0,0 +1,40 @@
+<?php
+$chars = [
+    'a' => 'e',
+    'o' => 'u',
+    'p' => 'b',
+    't' => 'd',
+    's' => 'c',
+    'j' => 'g',
+    'm' => 'n'
+];
+function encrypt(String $sentence){
+    $sentenceToLower = strtolower($sentence);
+    $encrypted_text = "";
+
+    $sentenceToLower  = 'acdc';
+
+    for ($i = 0; $i < strlen($sentenceToLower); $i++) {
+        // If we find the character in our mapping array, use the mapped character.
+        // If not, let's use the sente$sentenceToLower character.
+        $encrypted .= array_key_exists($sentenceToLower[$i], $chars) 
+            ? $chars[$sentenceToLower[$i]]
+            : $sentenceToLower[$i];
+    }        
+}
+
+function decrypt(String $encrypted){
+    // Decrypt
+    $decryptedText = '';
+    for ($i = 0; $i < strlen($encrypted); $i++) {
+        // Find the correct key
+        $key = array_search($encrypted[$i], $chars);
+
+        // If the character existed, use the key.
+        // If not, use the original character.
+        $decryptedText .= $key !== false 
+            ? $key
+            : $encrypted[$i];
+    }
+}
+?>
